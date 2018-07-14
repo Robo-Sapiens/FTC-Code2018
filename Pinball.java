ee81f67ffc81ad5a7e58732cbbd0fd4a66330371
Robo-Sapiens <40647677+Robo-Sapiens@users.noreply.github.com>
2018-07-13 20:28:36 -0700

    Add files via upload
    pinball machine code for outreach

---
+++ Pinball.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.team13180;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.team13180.LoaderArm;
+import org.firstinspires.ftc.teamcode.ManualOpMode13180;
+
+/**
+ * Created by Shivam Adeshara on 7/13/2018.
+ */
+@TeleOp
+public class Pinball extends ManualOpMode13180 {
+    private Servo leftServo;
+    private Servo rightServo;
+
+
+    @Override
+    public void runOpMode() {
+        //imu = hardwareMap.get(Gyroscope.class, "imu");
+        leftServo = hardwareMap.get(Servo.class, "leftServo");
+        rightServo = hardwareMap.get(Servo.class, "rightServo");
+        //servoTest2 = hardwareMap.get(Servo.class, "servoTest2");
+
+        // TODO: If we comment this line, then we need to change
+        // leftMote set[power to + instead of -
+        // Do after Saturday game
+        // One more direction is set to REVERSE
+        // leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+
+        // TODO: If we comment this line, then we need to change
+
+        // leftMote set[power to + instead of -
+        // Do after Saturday game
+        // One more direction is set to REVERSE
+        // leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        if (gamepad1.left_bumper) {
+            // move to 0 degrees.
+            leftServo.setPosition(1);
+            rightServo.setPosition(0);
+        } else if (gamepad1.right_bumper) {
+            leftServo.setPosition(0);
+            rightServo.setPosition(1);
+        }
+    }
+}
