11f1fb7c092eaf58f5f026c4f89f0ec6563e9036
shivamadeshara <32663087+shivamadeshara@users.noreply.github.com>
2018-07-27 19:48:57 -0700

    Add files via upload
    Final Pinball Code

--- Pinball.java
+++ Pinball.java
@@ -2,9 +2,7 @@ package org.firstinspires.ftc.team13180;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-//import org.firstinspires.ftc.team13180.LoaderArm;
 //import org.firstinspires.ftc.teamcode.ManualOpMode13180;
 
 /**
@@ -22,34 +20,30 @@ public class Pinball extends LinearOpMode {
         //imu = hardwareMap.get(Gyroscope.class, "imu");
         leftServo = hardwareMap.get(Servo.class, "leftServo");
         rightServo = hardwareMap.get(Servo.class, "rightServo");
-        //servoTest2 = hardwareMap.get(Servo.class, "servoTest2");
 
-        // TODO: If we comment this line, then we need to change
-        // leftMote set[power to + instead of -
-        // Do after Saturday game
-        // One more direction is set to REVERSE
-        // leftMotor.setDirection(DcMotor.Direction.REVERSE);
-
+        // Waiting for Run button press
         waitForStart();
 
+        // This will be in loop till we stop program
         while (opModeIsActive()) {
-            // TODO: If we comment this line, then we need to change
 
-            // leftMote set[power to + instead of -
-            // Do after Saturday game
-            // One more direction is set to REVERSE
-            // leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
             if (gamepad1.left_bumper) {
                 // move to 0 degrees.
                 leftServo.setPosition(0.5);
-                rightServo.setPosition(0.75);
-            } else if (gamepad1.right_bumper) {
+                sleep(200);
                 leftServo.setPosition(0);
+
+            } else if (gamepad1.right_bumper) {
+
                 rightServo.setPosition(0.25);
+                sleep(200);
+                rightServo.setPosition(0.85);
             }
             telemetry.addData("Status ",  "Running");
             telemetry.update();
         }
+
         telemetry.addData("Status ",  "Done");
         telemetry.update();
     }
