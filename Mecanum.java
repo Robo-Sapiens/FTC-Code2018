5747cf269a6f2e6bf82407a984fdd106ebfcac2f
Robo-Sapiens <40647677+Robo-Sapiens@users.noreply.github.com>
2018-08-05 15:52:52 -0700

    Add files via upload
    New Mecanum wheels

---
+++ Mecanum.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.team13180;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp
+public class Mecanum extends LinearOpMode {
+
+    private DcMotor Topl;
+    private DcMotor Topr;
+    private DcMotor Rearl;
+    private DcMotor Rearr;
+
+    @Override
+    public void runOpMode() {
+        //imu = hardwareMap.get(Gyroscope.class, "imu");
+        Topl = hardwareMap.get(DcMotor.class, "Topl");
+        Topr = hardwareMap.get(DcMotor.class, "Topr");
+        Rearl = hardwareMap.get(DcMotor.class, "Rearl");
+        Rearr = hardwareMap.get(DcMotor.class, "Rearr");
+
+
+
+
+
+        waitForStart();
+        double tgtPower1 = 0;
+        double tgtPower2 = 0;
+        double multiPlier1 = 0.50;
+        double multiPlier2 = 0.80;
+        while (opModeIsActive()) {
+            // Gamepad 1            // MotorTest1
+            tgtPower1 =  - multiPlier1 * this.gamepad1.left_stick_y;
+            Topl.setPower(tgtPower1);
+            // MotorTest2
+            tgtPower1 =  multiPlier1 * this.gamepad1.left_stick_y;
+            //tgtPower1 = - multiPlier1 * this.gamepad1.right_stick_y;
+            Topr.setPower(tgtPower1);
+            // Gamepad 2
+            // Gamepad 2 is used to control Motor 3 and 4
+            tgtPower2 =  multiPlier2 * this.gamepad2.left_stick_y;
+
+            //tgtPower2 = - multiPlier2 * this.gamepad2.right_stick_y;
+            //motorTest4.setPower(tgtPower2);
+            // check to see if we need to move the servo.
+            // Gamepad 2 is used to control Servo Motor 1 and 2
+
+
+            while (opModeIsActive()){
+
+        }
+
+    }
+
+    }}
+
+
+
+
+
+
+
+
+
